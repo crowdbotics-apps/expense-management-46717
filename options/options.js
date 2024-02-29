9cf5cff273ee65555ca8ff6858c2fe8f4ca57011
Crowdbotics <team@crowdbotics.com>
2024-02-29 17:47:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "expense-management-46717",
+  url: "https://expense-management-46717.botics.co",
+  api: "https://expense-management-46717.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
